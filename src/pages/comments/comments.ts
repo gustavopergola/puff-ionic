4bcffee75ba2e160bb4b41feead0d9c9960e0c67
vipkry <lolpsxvipkry2@gmail.com>
2017-11-05 01:02:40 -0200

    teacher comments are nw responding individually

--- src/pages/comments/comments.ts
+++ src/pages/comments/comments.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TeacherService } from '../../domain/teacher/teacher-service';
 import { UsuarioService } from '../../domain/usuario/usuario-service';
+import { AfterViewChecked } from '@angular/core';
 
 @IonicPage()
 @Component({
@@ -13,7 +14,8 @@ import { UsuarioService } from '../../domain/usuario/usuario-service';
 export class CommentsPage {
   
   public aux: any;
-  
+  public numero: number = 1;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private _service: UsuarioService, private _serviceTeacher: TeacherService) {
     this.getFeedback();
   }
@@ -32,18 +34,27 @@ export class CommentsPage {
   }
 
   
-  openModal(){
-    var div = document.querySelector('.comment-expandir');
-    var div2 = document.querySelector('.comment-delimitador');
-    var fa = document.querySelector('.fa-angle-down');
-    var fa2 = document.querySelector('.fa-angle-up');
+  openModal(id){
+    var div = document.querySelector('.comment-expandir-' + id);
+    var div2 = document.querySelector('#comment-delimitador-' + id);
+    var fa = document.querySelector('.fa-angle-down-' + id);
+    var fa2 = document.querySelector('.fa-angle-up-' + id);
+
     if (fa){
       div.setAttribute("style", "margin-top:0px!important;");  
-      fa.setAttribute("class","fa fa-angle-up");
+      fa.setAttribute("class","fa fa-angle-up fa-angle-up-" + id);
     }else{
       div.setAttribute("style", "margin-top:-180px!important;");  
-      fa2.setAttribute("class","fa fa-angle-down");
+      fa2.setAttribute("class","fa fa-angle-down fa-angle-down-" + id);
     }
   }
-  
+
+  ngAfterViewChecked (){
+    var counters = document.getElementsByClassName("counter");
+    console.log(counters);
+    for (var index = 0; index < counters.length; index++) {
+      counters[index].innerHTML = (index + 1).toString(); 
+    }
+  }
+
 }
