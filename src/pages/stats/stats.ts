6b7992e68a32243bbdb02b549e6c9a049101f57b
vipkry <lolpsxvipkry2@gmail.com>
2017-11-05 03:23:08 -0200

    código identado e nome personalizado

--- src/pages/stats/stats.ts
+++ src/pages/stats/stats.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Chart} from 'chart.js';
 
-/**
- * Generated class for the StatsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-stats',
@@ -16,77 +9,72 @@ import {Chart} from 'chart.js';
 })
 export class StatsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  chartFoco(){
-      var ctx =document.querySelector('#myChart');
-      new Chart(ctx, {
-    type: 'line',
-    data:  {
-        labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul","Ago","Set","Out","Nov","Dez"],
-        datasets: [{
-            label: 'Tabela de quantidade de pessoas que focam na aula',
-            data: [1.3, 2.1, 2.3, 2.3, 2.4,2.5,2.8,3.0,3.3,3.6,4],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        elements: {
-            line: {
-                tension: 0.1,
-            }
-        }
-    }
-});
-
-  }
-  
-    chartFoco2(){
-      var ctx =document.querySelector('#myChart');
-      new Chart(ctx, {
-        type: 'line',
-        data:  {
-            labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul","Ago","Set","Out","Nov","Dez"],
-            datasets: [{
-                label: 'Tabela de qualidade do material',
-                data: [2.5,2.8,3.0,3.3,1.3, 2.1, 2.3, 2.3, 2.4,3.6,4,4.7],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            elements: {
-                line: {
-                    tension: 0.1,
+    chartFoco(){
+        var ctx =document.querySelector('#myChart');
+        new Chart(ctx, {
+            type: 'line',
+            data:  {
+                labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul","Ago","Set","Out","Nov","Dez"],
+                datasets: [{
+                    label: 'Tabela de quantidade de pessoas que focam na aula',
+                    data: [1.3, 2.1, 2.3, 2.3, 2.4,2.5,2.8,3.0,3.3,3.6,4],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                elements: {
+                    line: {
+                        tension: 0.1,
+                    }
                 }
             }
-        }
-});
-
-  }
-    chartFoco3(){
-      var ctx =document.querySelector('#myChart');
-      new Chart(ctx, {
-        type: 'line',
-        data:  {
-            labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul","Ago","Set","Out","Nov","Dez"],
-            datasets: [{
-                label: 'Tabela de presença do professor',
-                data: [2.5, 2.8, 3.0, 3.3, 1.3, 2.1, 2.3, 2.3, 2.4, 3.6, 4, 2.1, 2.3, 2.3, 3.8],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            elements: {
-                line: {
-                    tension: 0.1,
+        });
+    }
+  
+    chartMaterial(){
+        var ctx =document.querySelector('#myChart');
+        new Chart(ctx, {
+            type: 'line',
+            data:  {
+                labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul","Ago","Set","Out","Nov","Dez"],
+                datasets: [{
+                    label: 'Tabela de qualidade do material',
+                    data: [2.5,2.8,3.0,3.3,1.3, 2.1, 2.3, 2.3, 2.4,3.6,4,4.7],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                elements: {
+                    line: {
+                        tension: 0.1,
+                    }
                 }
             }
+        });
+
     }
-});
-
-  }
-
- 
+    
+    chartPresenca(){
+        var ctx =document.querySelector('#myChart');
+        new Chart(ctx, {
+            type: 'line',
+            data:  {
+                labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul","Ago","Set","Out","Nov","Dez"],
+                datasets: [{
+                    label: 'Tabela de presença do professor',
+                    data: [2.5, 2.8, 3.0, 3.3, 1.3, 2.1, 2.3, 2.3, 2.4, 3.6, 4, 2.1, 2.3, 2.3, 3.8],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                elements: {
+                    line: {
+                        tension: 0.1,
+                    }
+                }
+            }
+        });
+    }
 }
